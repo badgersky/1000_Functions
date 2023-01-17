1140b75329af8b2ee55536c77152bd947877c8f3
badgersky <szymonek.ptasznik@gmail.com>
2023-01-17 10:28:03 +0100

    rectangle area function

---
+++ main.py
@@ -0,0 +1,8 @@
+def rectangle_area(a, b):
+    """
+
+    :param a: int/float: length of one side
+    :param b: int/float: length of other side
+    :return: float/int: area of rectangle
+    """
+    return a * b
