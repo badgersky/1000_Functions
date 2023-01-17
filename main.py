ad7cf5ca1a6ab5f14ede12e0c82ea8b46ee763c9
badgersky <szymonek.ptasznik@gmail.com>
2023-01-17 10:30:54 +0100

    circle area function

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from math import pi
+
+
 def rectangle_area(a, b):
     """
 
@@ -6,3 +9,14 @@ def rectangle_area(a, b):
     :return: float/int: area of rectangle
     """
     return a * b
+
+
+def circle_area_from_radius(r):
+    """
+
+    :param r: float/int: circle radius
+    :return: float: circle area
+    """
+    return pi * r ** 2
+
+
