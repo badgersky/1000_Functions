e8c11aa64662c0b032994656def137a8303f5303
badgersky <szymonek.ptasznik@gmail.com>
2023-07-13 16:41:53 +0200

    create function that counts all words in text

--- main.py
+++ main.py
@@ -807,6 +807,17 @@ def count_word_occurences2(word, text):
         if word == s:
             counter += 1
     return counter
+  
 
+def count_words(text):
+    """
+    Function counts number of words in text
+    
+    :param text: str: text to count words of
+    :return: int: number of words in text
+    """
 
-print(count_word_occurences2('WE', 'We and our partners store and/or access information on a device, such as cookies and process personal data, such as unique identifiers and standard information sent by a device for personalised ads and content, ad and content measurement, and audience insights, as well as to develop and improve products. With your permission we and our partners may use precise geolocation data and identification through device scanning. You may click to consent to our and our partners’ processing as described above. Alternatively you may click to refuse to consent or access more detailed information and change your preferences before consenting. Please note that some processing of your personal data may not require your consent, but you have a right to object to such processing. Your preferences will apply to this website only. You can change your preferences at any time by returning to this site or visit our privacy policy.'))
+    text = clear_text(text)
+    return len(text.split())
+
+print(count_words('We and our partners store and/or access information on a device, such as cookies and process personal data, such as unique identifiers and standard information sent by a device for personalised ads and content, ad and content measurement, and audience insights, as well as to develop and improve products. With your permission we and our partners may use precise geolocation data and identification through device scanning. You may click to consent to our and our partners’ processing as described above. Alternatively you may click to refuse to consent or access more detailed information and change your preferences before consenting. Please note that some processing of your personal data may not require your consent, but you have a right to object to such processing. Your preferences will apply to this website only. You can change your preferences at any time by returning to this site or visit our privacy policy.'))
