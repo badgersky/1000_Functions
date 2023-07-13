7cf80489b31053c4b2401d026f4e5729afc0f70b
badgersky <szymonek.ptasznik@gmail.com>
2023-07-13 16:57:29 +0200

    create function that returns dictionary with encoding rules, create funciton that encodes message according to rules

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 from math import pi, atan
 from random import randint
+from string import ascii_lowercase
 
 
 def rectangle_area(a, b):
@@ -820,4 +821,31 @@ def count_words(text):
     text = clear_text(text)
     return len(text.split())
 
-print(count_words('We and our partners store and/or access information on a device, such as cookies and process personal data, such as unique identifiers and standard information sent by a device for personalised ads and content, ad and content measurement, and audience insights, as well as to develop and improve products. With your permission we and our partners may use precise geolocation data and identification through device scanning. You may click to consent to our and our partners’ processing as described above. Alternatively you may click to refuse to consent or access more detailed information and change your preferences before consenting. Please note that some processing of your personal data may not require your consent, but you have a right to object to such processing. Your preferences will apply to this website only. You can change your preferences at any time by returning to this site or visit our privacy policy.'))
+
+def encoding_rules():
+    rules = {char: ascii_lowercase[-i] for i, char in enumerate(ascii_lowercase, start=1)}
+    return rules
+
+
+def encode_message(message):
+    """
+    Function encodes message according to encoding rules
+
+    :param message: str: message to encode
+    :return: str: encoded message
+    """
+    rules = encoding_rules()
+
+    message = clear_text(message)
+    message = message.lower()
+
+
+    encoded_message = ''
+    for char in message:
+        encoded_message += rules[char]
+
+    return encoded_message
+
+
+print(encode_message('aaa'))
+print(encode_message('siema'))
